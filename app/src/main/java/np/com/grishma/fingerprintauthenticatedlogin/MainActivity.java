39db62f464379dbf29f8d4d41480c1c7bd17d6d2
Grishma Shrestha <grishmashrestha@lftechnology.com>
2016-09-22 09:01:25 +0545

    Check for hardware, enrolled fingerprints and finger print permission

--- app/src/main/java/np/com/grishma/fingerprintauthenticatedlogin/MainActivity.java
+++ app/src/main/java/np/com/grishma/fingerprintauthenticatedlogin/MainActivity.java
@@ -1,18 +1,22 @@
 package np.com.grishma.fingerprintauthenticatedlogin;
 
+import android.Manifest;
 import android.app.KeyguardManager;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.hardware.fingerprint.FingerprintManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.security.keystore.KeyGenParameterSpec;
 import android.security.keystore.KeyProperties;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
@@ -54,6 +58,8 @@ public class MainActivity extends AppCompatActivity {
     private KeyPairGenerator keyPairGenerator;
     private SharedPreferences sharedPreferences;
     private User user = new UserImpl();
+    private KeyguardManager keyguardManager;
+    private FingerprintManager fingerprintManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,8 +68,8 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         // initialize required parameters
-        KeyguardManager keyguardManager = getSystemService(KeyguardManager.class);
-        FingerprintManager fingerprintManager = getSystemService(FingerprintManager.class);
+        keyguardManager = getSystemService(KeyguardManager.class);
+        fingerprintManager = getSystemService(FingerprintManager.class);
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
         // get an instance of keystore
@@ -80,6 +86,26 @@ public class MainActivity extends AppCompatActivity {
             throw new RuntimeException("Failed to get an instance of KeyPairGenerator", e);
         }
 
+        if (!keyguardManager.isKeyguardSecure()) {
+            // Show a message that the user hasn't set up a fingerprint or lock screen.
+            Toast.makeText(this,
+                    "Secure lock screen hasn't set up.\n"
+                            + "Go to 'Settings -> Security -> Fingerprint' to set up a fingerprint",
+                    Toast.LENGTH_LONG).show();
+            checkBoxFingerprintAuth.setVisibility(View.INVISIBLE);
+            return;
+        }
+
+        //noinspection ResourceType
+        if (!fingerprintManager.hasEnrolledFingerprints()) {
+            checkBoxFingerprintAuth.setVisibility(View.INVISIBLE);
+            // This happens when no fingerprints are registered.
+            Toast.makeText(this,
+                    "Go to 'Settings -> Security -> Fingerprint' and register at least one fingerprint",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+
         if (sharedPreferences.getBoolean("fingerprintEnabled", false)) {
             startActivity(new Intent(this, FingerprintLoginActivity.class));
             finish();
@@ -93,7 +119,10 @@ public class MainActivity extends AppCompatActivity {
         String passwordString = password.getText().toString();
         // verify user in the server
         if (user.verify(usernameString, passwordString)) {
-            if (checkBoxFingerprintAuth.isChecked()) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            if (checkBoxFingerprintAuth.isChecked() && fingerprintManager.hasEnrolledFingerprints()) {
                 // create a keypair
                 createKeyPair();
                 // enroll process below:
